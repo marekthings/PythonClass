b5c594c11850e4901e4afb3fa70839c4fdce99e6
marekthings <49995993+marekthings@users.noreply.github.com>
2020-02-18 11:46:13 -0600

    Add files via upload

---
+++ Day 1/Day1 Home Assignment.py
@@ -0,0 +1,9 @@
+#Q1. Write a python code to print "Python is Fun"
+
+#Q2. Write a python code to print "This is an example to display display backslash character \"
+
+#Q3. Write a python code to print "Hello" on first Line and "World" on second line
+#Hint: Use one of Escape Sequence to print value in new line
+
+
+#Extra Credit: Write a python code to print sum of two numbers 5 & 8

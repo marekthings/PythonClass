d1b25d3f75f948d8ad7a0e0f93929d2f5c2d090b
marekthings <49995993+marekthings@users.noreply.github.com>
2020-02-19 14:29:11 -0600

    Add files via upload

---
+++ Day 2/Day2 Home Assignment.py
@@ -0,0 +1,17 @@
+#Q1. Write a python program to input two numbers from the user and print its addition.
+
+
+
+#Q2. Write a python program to input name, age, address and print the details in one line
+
+
+
+#Q3. Write a python program to input a number from user and check if the number is even or odd
+
+
+
+
+#Q4. Write a python program to input the number of drivers in their household.
+#Each driver costs 70 per month for insurance, but receives a 10 dollar discountfor every 3 drivers, calculate the total cost
+
+
